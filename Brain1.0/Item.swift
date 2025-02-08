d54f63bf79c9b82cb60d4ac3ad5e17ac28e6bc74
JDeWeese <jdeweese574@icloud.com>
2025-02-08 12:04:03 -0600

    Initial Commit

---
+++ Brain1.0/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Brain1.0
+//
+//  Created by Joseph DeWeese on 2/8/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Brain1.0Tests/Brain1_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Brain1_0Tests.swift
+//  Brain1.0Tests
+//
+//  Created by Joseph DeWeese on 2/8/25.
+//
+
+import Testing
+@testable import Brain1_0
+
+struct Brain1_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
